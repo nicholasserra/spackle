94de9869a9f0f32fa98c6a15d237f987907a8686
Nicholas Serra <nickserra@gmail.com>
2020-10-20 20:47:47 -0400

    setup.py classifiers and requires

--- setup.py
+++ setup.py
@@ -19,11 +19,25 @@ setup(
         'console_scripts': [
             'spackle = spackle.script:main',
     ]},
+    python_requires=">=2.7, <4",
     classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ]
 )
